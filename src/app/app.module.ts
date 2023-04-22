6fb1ca80e7494c39f87b7f5c2f36fe063f9bbfce
arturSzuta <artszuta@hotmail.com>
2023-04-22 14:51:09 +0200

    Second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsListComponent } from './students-list/students-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, StudentsListComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/students-list/students-list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { StudentService } from '../student.service';
+import { Student } from '../model/student';
+
+@Component({
+  selector: 'app-students-list',
+  templateUrl: './students-list.component.html',
+  styleUrls: ['./students-list.component.css'],
+})
+export class StudentsListComponent {
+  students: Student[] | undefined;
+  constructor(private studentService: StudentService) {
+    this.studentService.getStudents().subscribe((data) => {
+      this.students = data;
+      console.log(this.students);
+    });
+  }
+}
